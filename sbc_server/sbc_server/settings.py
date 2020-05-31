f2975fae411e7ed42dc8baafb3b18b1f9018819e
EC2 Default User <ec2-user@ip-10-0-0-38.eu-central-1.compute.internal>
2020-05-31 15:26:52 +0000

    syntax fix

--- sbc_server/sbc_server/settings.py
+++ sbc_server/sbc_server/settings.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = (
 )
 
 if os.getenv("MODE") == "production":
-    AWS_STORAGE_BUCKET_NAME = f'sorterbot-static-{ssm.get_parameter(Name='RESOURCE_SUFFIX', WithDecryption=True)['Parameter']['Value']}'
+    AWS_STORAGE_BUCKET_NAME = f"sorterbot-static-{ssm.get_parameter(Name='RESOURCE_SUFFIX', WithDecryption=True)['Parameter']['Value']}"
     AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
